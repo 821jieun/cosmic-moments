441093968279c5793c2d21b1a4579d1382aa5966
jieun <821jieun@gmail.com>
2018-03-07 14:28:56 -0500

    added date picker fallback

--- index.js
+++ index.js
@@ -3,77 +3,167 @@
 const API_KEY = 'UO59leZhmMDtmeu9Fp5nTPOCDuMmYFD63bJbFSBU';
 const rootUrl = 'https://api.nasa.gov/planetary/apod?hd=True';
 
+//get today's date
+window.onload = todaysDate();
+
 //helper functions
+function placeholderSuggestions() {
+  const searchExamples = [ 'Want some suggestions?', 'shooting stars', 'quasars', 'iridescent clouds', 'leo triplet', 'globular cluster', 'great comet', 'solar wind', 'nebula', 'From the Earth to the Moon', 'helix', 'neutron star', 'supernova', 'cosmic dust', 'electromagnetic radiation', 'paradigm shift', 'summer triangle', 'nova delphini' ];
+  setInterval(function() {
+    $("input#wiki-query").attr("placeholder", searchExamples[searchExamples.push(searchExamples.shift())-1]);
+  }, 3000);
+}
+
+
 function todaysDate() {
-  let date = new Date();
-  date = new Array(date);
-  date = date.slice(2, 5);
-  return date;
-}
-
-function showErr(err) {
-  const outputElem = $(".js-search-results");
-  
-  const errMsg = (
-    `<p>Please try a different date! Nothing is returned for that entry.</p>`
-  );
-    
-  outputElem
-    .prop('hidden', false)
-    .html(errMsg);
-}
-
-function convertDate(dateString) {
-    const monthNames = [
+  const monthNames = [
     "January", "February", "March",
     "April", "May", "June", "July",
     "August", "September", "October",
     "November", "December"
   ];
-  
-    let monthName = dateString.slice(0, 2).toLowerCase();
-    const index = monthNames.findIndex(month => month.slice(0, 2).toLowerCase() === monthName);
-    const monthNum = index + 1;
-    const year = parseInt(dateString.slice(-4))
-    const date = parseInt(dateString.split(" ")[1]);
+  let n = new Date();
+  const y = n.getFullYear();
+  const m = n.getMonth();
+  const month = monthNames[m];
+  const d = n.getDate();
+  let date = `${month} ${d} ${y}`;
+  $(".todays-date").text(date);
+  date = convertDate(date);
+  $("#nasa-query").attr("max", date);
+
+}
+
+
+function convertDate(dateString) {
+
+    //if using date-picker
+   let year = parseInt(dateString.slice(0, 4));
+   console.log(typeof year)
+   let monthNum = parseInt(dateString.slice(6, 8));
+   let date = parseInt(dateString.slice(-2))
+
     return {
-      year: year, 
-      month: monthNum, 
+      year: year,
+      month: monthNum,
       date: date
     }
 }
 
-//handling wikipedia click link to open modal
-$(".js-wikipedia-search-results").on("click", "#modalBtn", openModal);
+//fallback date picker:https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/date#Browser_compatibility
+// define variables
+var nativePicker = document.querySelector('.nativeDatePicker');
+var fallbackPicker = document.querySelector('.fallbackDatePicker');
+var fallbackLabel = document.querySelector('.fallbackLabel');
 
-function openModal() {
-  const modal = $("#simpleModal");
-  console.log("open sesame!")
-  const url = $("a[data-url]");
-  //test it out
-  $("modal-content p").val(url);
-  modal.addClass(".displayBlock");
+var yearSelect = document.querySelector('#year');
+var monthSelect = document.querySelector('#month');
+var daySelect = document.querySelector('#day');
+
+// hide fallback initially
+fallbackPicker.style.display = 'none';
+fallbackLabel.style.display = 'none';
+
+// test whether a new date input falls back to a text input or not
+var test = document.createElement('input');
+test.type = 'date';
+
+// if it does, run the code inside the if() {} block
+if(test.type === 'text') {
+  // hide the native picker and show the fallback
+  nativePicker.style.display = 'none';
+  fallbackPicker.style.display = 'block';
+  fallbackLabel.style.display = 'block';
+
+  // populate the days and years dynamically
+  // (the months are always the same, therefore hardcoded)
+  populateDays(monthSelect.value);
+  populateYears();
 }
 
-//listen for modal close
-$(".modal-content").on("click", ".closeBtn", closeModal);
+function populateDays(month) {
+  // delete the current set of <option> elements out of the
+  // day <select>, ready for the next set to be injected
+  while(daySelect.firstChild){
+    daySelect.removeChild(daySelect.firstChild);
+  }
 
-function closeModal() {
-  const modal = $("#simpleModal");
-  modal.addClass(".displayNone");
+  // Create variable to hold new number of days to inject
+  var dayNum;
+
+  // 31 or 30 days?
+  if(month === 'January' || month === 'March' || month === 'May' || month === 'July' || month === 'August' || month === 'October' || month === 'December') {
+    dayNum = 31;
+  } else if(month === 'April' || month === 'June' || month === 'September' || month === 'November') {
+    dayNum = 30;
+  } else {
+  // If month is February, calculate whether it is a leap year or not
+    var year = yearSelect.value;
+    (year - 2016) % 4 === 0 ? dayNum = 29 : dayNum = 28;
+  }
+
+  // inject the right number of new <option> elements into the day <select>
+  for(i = 1; i <= dayNum; i++) {
+    var option = document.createElement('option');
+    option.textContent = i;
+    daySelect.appendChild(option);
+  }
+
+  // if previous day has already been set, set daySelect's value
+  // to that day, to avoid the day jumping back to 1 when you
+  // change the year
+  if(previousDay) {
+    daySelect.value = previousDay;
+
+    // If the previous day was set to a high number, say 31, and then
+    // you chose a month with less total days in it (e.g. February),
+    // this part of the code ensures that the highest day available
+    // is selected, rather than showing a blank daySelect
+    if(daySelect.value === "") {
+      daySelect.value = previousDay - 1;
+    }
+
+    if(daySelect.value === "") {
+      daySelect.value = previousDay - 2;
+    }
+
+    if(daySelect.value === "") {
+      daySelect.value = previousDay - 3;
+    }
+  }
 }
 
-//listen for background click
-// window.on("click", clickOutside);
+function populateYears() {
+  // get this year as a number
+  var date = new Date();
+  var year = date.getFullYear();
 
-// function clickOutside(e) {
-//   const modal = $(".simpleModal");
-//   if (e.target == modal) {
-//     modal.addClass("displayNone") 
-//   }
-  
-// } 
+  // Make this year, and the 100 years before it available in the year <select>
+  for(var i = 0; i <= 100; i++) {
+    var option = document.createElement('option');
+    option.textContent = year-i;
+    yearSelect.appendChild(option);
+  }
+}
 
+// when the month or year <select> values are changed, rerun populateDays()
+// in case the change affected the number of available days
+yearSelect.onchange = function() {
+  populateDays(monthSelect.value);
+}
+
+monthSelect.onchange = function() {
+  populateDays(monthSelect.value);
+}
+
+//preserve day selection
+var previousDay;
+
+// update what day has been set to previously
+// see end of populateDays() for usage
+daySelect.onchange = function() {
+  previousDay = daySelect.value;
+}
 
 //listen for wikipedia search submission
 $('.wikipedia-search-form').on('click', '.wiki-button', handleWikipediaFormSubmit);
@@ -85,17 +175,16 @@ function handleWikipediaFormSubmit() {
     let searchTerm = $("#wiki-query").val();
     $("#wiki-query").val('');
     getWikipediaSearchResults(searchTerm);
-   
+
   });
 }
 
 function getWikipediaSearchResults(searchTerm) {
   const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchTerm}&inprop=url&utf8=&format=json`;
-  console.log(url);
-  console.log('inside getWikipediaSearchResults');
+
     $.ajax({
   	url: url,
-  	dataType: 'jsonp', 
+  	dataType: 'jsonp',
   	type: 'POST',
     headers: { 'Api-User-Agent': 'Example/1.0' },
   	success: function(data) {
@@ -106,31 +195,54 @@ function getWikipediaSearchResults(searchTerm) {
 }
 
 function renderWikiSearchResults(data){
+  var body = $("#iframe").contents().find("body");
 
   const output = $(".js-wikipedia-search-results");
   const results = data.query.search.map((item, index) => htmlifyWikiResults(item));
 
   output
   .prop('hidden', false)
+
+  body
   .html(results);
 }
 
 function htmlifyWikiResults(data) {
   const {title, snippet } = data;
   const url = title.split(' ').join('_');
- 
-    return `
-    <p><a id="modalBtn" data-url="https://en.wikipedia.org/wiki/${url}" class="button" alt="link to ${title} article" href="https://en.wikipedia.org/wiki/${url}">${title}</a></p><p>${snippet}</p>
+
+  //CORS?
+      // return `
+      // <video role="application" crossorigin="anonymous" alt="${title}" src="${url}" controls ></video><p>${explanation}</p>
+      //   <p>copyright: ${copyright}</p>
+      // `
+
+  //why can't height of iframe be adjusted?
+  return `
+      <p><a alt="link to ${title} article" href="https://en.wikipedia.org/wiki/${url}">${title}</a><div class="box"><iframe src="https://en.wikipedia.org/wiki/${url}" width = "100%" height="70%" height ="500px"></iframe></div></p>
       `;
+
 }
 
-//nasa search form 
+//nasa search form
 function htmlifyNasaResults(data) {
-    const { copyright="NASA", explanation, hdurl, title, url } = data;
-    return `
-      <a href="${hdurl}" target="_blank" alt="${title}"><img alt="${title}" src="${url}"></a><p>${explanation}</p>
-      <p>copyright: ${copyright}</p>
-      `;
+    const { copyright="NASA", explanation, hdurl, title, url, media_type } = data;
+
+    if (media_type === "image") {
+
+      return `
+        <a href="${hdurl}" target="_blank" alt="${title}"><img alt="${title}" src="${url}"></a><p>${explanation}</p>
+        <p>copyright: ${copyright}</p>
+        `;
+    }
+
+    if (media_type === "video") {
+      return `
+      <iframe class="nasa-video" alt="${title}" src="${url}" width="90%" ></iframe><p>${explanation}</p>
+        <p>copyright: ${copyright}</p>
+      `
+    }
+
 }
 
 
@@ -142,9 +254,11 @@ function renderNasaSearchResults(data) {
     output
     .prop('hidden', false)
     .html(results);
-    
+
     wikiSearchForm
     .prop('hidden', false);
+
+    placeholderSuggestions();
 }
 
 
@@ -153,28 +267,43 @@ function handleNasaSubmitForm() {
   $("#js-nasa-search-form").submit((e) => {
     event.preventDefault();
     let searchDate = $("#nasa-query").val();
+    //   console.log(typeof searchDate, searchDate);
     searchDate = convertDate(searchDate);
-    // console.log(searchDate);
+
     $("input").val('');
     getAstronomyPictureOfTheDay(rootUrl, searchDate)
 
   });
-  
-}
 
+}
 
 function getAstronomyPictureOfTheDay(rootUrl, searchDate) {
 
-	const url = `${rootUrl}&date=${searchDate.year}-${searchDate.month}-${searchDate.date}&api_key=${API_KEY}`;	
+	const url = `${rootUrl}&date=${searchDate.year}-${searchDate.month}-${searchDate.date}&api_key=${API_KEY}`;
   console.log(url)
   $.ajax({
   	url: url,
-  	dataType: 'json', 
-  	type: 'GET', 
+  	dataType: 'json',
+  	type: 'GET',
   	success: function(data) {
-  		// console.log(data)
+  		console.log(data)
   		renderNasaSearchResults(data);
-  	}
+  	},
+  error: function(request,status,errorThrown) {
+
+  const outputElem = $(".js-search-results");
+  outputElem
+    .prop('hidden', false)
+    .html(`${errorThrown}`);
+  // const errMsg = (
+  //   `<p>Please try a different date! Nothing is returned for that entry.</p>`
+  // );
+
+  // outputElem
+  //   .prop('hidden', false)
+  //   .html(errMsg);
+
+   }
   });
 
 }
